fdb5eec35c192dd7f6568e53e9621455b84dd562
thanakorn <thanakorn.panyapiang@outlook.com>
2020-07-06 13:30:35 +0100

    GymFitnessEvaluator

--- genetic_algorithm/fitness_evaluator.py
+++ genetic_algorithm/fitness_evaluator.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 from typing import TypeVar, Generic
-from torch.utils.data import DataLoader
 from genetic_algorithm.genotype import NetworkGenotype
 from model.genetic_network import GeneticNetwork
+from itertools import count
 
 T = TypeVar('T', GeneticNetwork, GeneticNetwork)
 
@@ -11,24 +11,29 @@ class FitnessEvaluator():
     def eval_fitness(self, genotype: NetworkGenotype) -> float:
         raise NotImplementedError()
     
-class DatasetFitnessEvaluator():
-    def __init__(self, dataset, batch_size=128, shuffle=True, num_workers=0, device='cpu'):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.num_workers = num_workers
+class GymFitnessEvaluator():
+    def __init__(self, env, frame_skip=1, device='cpu'):
+        self.env = env
+        self.frame_skip = frame_skip
         self.device = device
         
-    def eval_fitness(self, model_type: T, genotype: NetworkGenotype):
-        fitness = 0.
+    def eval_fitness(self, model_type: T, genotype: NetworkGenotype, num_episodes=1, visualize = False):
         model = model_type(genotype).to(self.device)
         model.eval()
-        dataloader = DataLoader(dataset=self.dataset, batch_size=self.batch_size, 
-                                shuffle=self.shuffle, num_workers=self.num_workers)
-        for data in dataloader:
-            inputs, labels = data[0].to(self.device), data[1].to(self.device)
-            predicts = model(inputs)
-            predicts = torch.argmax(predicts, dim=1)
-            fitness += (predicts == labels).sum()
-        return fitness / len(self.dataset)
-    
+        fitness = 0.
+        
+        for ep in range(num_episodes):
+            state = self.env.reset()
+            done = False
+            total_reward = 0
+            
+            while not done:
+                if visualize : self.env.render()
+                action = model(state.unsqueeze(0)).argmax()
+                state, reward, done, _ = self.env.step(action)
+                total_reward += reward.item()
+            
+            fitness += total_reward / float(num_episodes)
+            
+        self.env.close()
+        return fitness
