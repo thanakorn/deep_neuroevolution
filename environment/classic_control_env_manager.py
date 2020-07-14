ed0ecd6a7ec2f41f6dabd8f4d6bc7d7220328358
thanakorn <thanakorn.panyapiang@outlook.com>
2020-07-14 15:54:35 +0100

    Remove unused

--- environment/classic_control_env_manager.py
+++ environment/classic_control_env_manager.py
@@ -2,11 +2,18 @@ import gym
 import torch
 import numpy as np
 import cv2
+import torchvision.transforms as T
 
 from collections import deque
 from environment.environment_manager import EnvironmentManager
 
-IMAGE_SIZE = (60, 40)
+process_screen = T.Compose([
+                    T.ToPILImage(),
+                    T.Grayscale(num_output_channels=1),
+                    T.Resize((100, 150)),
+                    T.CenterCrop(64),
+                    T.ToTensor()
+                ])
 
 class ClassicControlEnvManager(EnvironmentManager):
     def __init__(self, env_name):
@@ -19,16 +26,15 @@ class ClassicControlEnvManager(EnvironmentManager):
     """Return a stack of tensors representing last k frames"""
     def state(self):
         screen = self.env.render('rgb_array')
-        screen = self.processed_screen(screen)
-        return torch.from_numpy(screen).float()
+        return self.processed_screen(screen)
     
     def step(self, action):
         _, reward, self.done, _ = self.env.step(action)
         return (self.state(), torch.tensor([reward]), self.done, _)
         
     def processed_screen(self, screen):
-        screen = cv2.cvtColor(screen, cv2.COLOR_RGB2GRAY)
-        screen = cv2.resize(screen, IMAGE_SIZE)
-        h, w = screen.shape
-        screen = np.ascontiguousarray(screen)
-        return screen
+        processed_screen = process_screen(screen).squeeze(0)
+        return processed_screen
+    
+    def get_raw_screen(self):
+        return self.env.render('rgb_array')
