fd1f4c9891c5247893b2ce47dad87c01eed7c08f
thanakorn <thanakorn.panyapiang@outlook.com>
2020-07-29 19:14:23 +0100

    Change env to parameter

--- genetic_algorithm/fitness_evaluator.py
+++ genetic_algorithm/fitness_evaluator.py
@@ -14,33 +14,30 @@ class FitnessEvaluator:
         raise NotImplementedError()
     
 class GymFitnessEvaluator(FitnessEvaluator):
-    def __init__(self, env_name, num_episodes=1, max_iterations=None, device='cpu', visualize=False):
-        self.env_name = env_name
-        self.num_episodes = num_episodes
-        self.max_iterations = max_iterations
-        self.device = device
-        self.visualize = visualize
+    def __init__(self, env_manger: EnvironmentManager, **env_args):
+        self.env_manger = env_manger
+        self.env_args = env_args
         
-    def eval_fitness(self, genotype: NetworkGenotype):
-        env = EnvironmentManager(self.env_name, device=self.device)
-        model = genotype.to_network().to(self.device)
-        model.eval()
-        fitness = 0.
+    def eval_fitness(self, genotype: NetworkGenotype, max_iterations, num_episodes=1, visualize=False):
+        env = self.env_manger(**self.env_args)
+        with torch.no_grad():
+            model = genotype.to_network().to(env.device)
+            fitness = 0.
 
-        for ep in range(self.num_episodes):
-            state = env.reset()
-            done = False
-            total_reward = 0.
-            num_iterations = 0
-            
-            while not done and (self.max_iterations is None or num_iterations < self.max_iterations):
-                if self.visualize : env.render()
-                action = model(state.unsqueeze(0)).argmax().item()
-                state, reward, done, _ = env.step(action)
-                total_reward += reward
-                num_iterations += 1
-            
-            fitness += total_reward / float(self.num_episodes)
-
+            for ep in range(num_episodes):
+                state = env.reset()
+                done = False
+                total_reward = 0.
+                num_iterations = 0
+                
+                while not done and (max_iterations is None or num_iterations < max_iterations):
+                    if visualize : env.render()
+                    action = model(state.unsqueeze(0)).argmax().item()
+                    state, reward, done, _ = env.step(action)
+                    total_reward += reward
+                    num_iterations += 1
+                
+                fitness += total_reward / float(num_episodes)
+        
         env.close()
         return fitness
