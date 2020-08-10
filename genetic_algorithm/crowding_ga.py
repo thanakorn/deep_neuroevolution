361277f7e018682592c95b4e4295f0ce27f2d241
thanakorn <thanakorn.panyapiang@outlook.com>
2020-08-10 18:59:43 +0100

    Fix runtime errors

--- genetic_algorithm/crowding_ga.py
+++ genetic_algorithm/crowding_ga.py
@@ -4,9 +4,9 @@ from genetic_algorithm.ga import GeneticAlgorithm
 from genetic_algorithm.opetators import crossover, mutate
 from utilities.random_generator import NPRandomGenerator
 from utilities.ga_helpers import calculate_fitnesses
-from utilities import random_generator
+from genetic_algorithm.opetators import random_generator
 
-class DeterministicCrowdingGA(CrowdingGA):    
+class DeterministicCrowdingGA(GeneticAlgorithm):    
     def run(self, populations, num_generations, max_iterations=None, num_episodes_eval=1, visualize=False, num_workers=None, run_mode=None):
         solution = None
         for gen in range(num_generations):
@@ -18,11 +18,12 @@ class DeterministicCrowdingGA(CrowdingGA):
     
     def new_generation(self, old_gen, fitnesses, *args):
         offsprings = []
+        gen, num_workers, run_mode, max_iterations, num_episodes_eval, visualize = args
         for p1, p2 in zip(old_gen[0::2], old_gen[1::2]):
             c1, c2 = crossover(p1, p2, random_generator)
             offsprings.append(mutate(c1, random_generator, self.mutation_prob, self.mutation_power))
             offsprings.append(mutate(c2, random_generator, self.mutation_prob, self.mutation_power))
-        offspring_fitnesses = calculate_fitnesses(offsprings, self.fitness_evaluator, args)
+        offspring_fitnesses = calculate_fitnesses(offsprings, self.fitness_evaluator, gen, num_workers, run_mode, max_iterations, num_episodes_eval, visualize)
         
         new_generation = []
         for i in range(self.num_populations):
