671b8aba91fe1a97e3ad70da46e9981bb13796e4
thanakorn <thanakorn.panyapiang@outlook.com>
2020-08-24 08:34:11 +0100

    Adjust style

--- image_maze_crowding.py
+++ image_maze_crowding.py
@@ -3,6 +3,8 @@ import torch
 import ray
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2 as cv
 
 from genetic_algorithm.genotype import TensorGenotype
 from genetic_algorithm.network_schema import *
@@ -10,6 +12,8 @@ from genetic_algorithm.crowding_ga import DeterministicCrowdingGA
 from genetic_algorithm.fitness_evaluator import GymFitnessEvaluator
 from environment.framestack_env_manager import FrameStackEnvManager
 from utilities.evaluation_logger import EvaluationLogger
+
+sns.set(style='darkgrid')
 
 def preprocess(screen):
     screen[screen < 255] = 0
@@ -44,25 +48,29 @@ evaluator = GymFitnessEvaluator(FrameStackEnvManager, eval_logger, env_name=env_
 init_populations = [TensorGenotype(network_schema, torch.nn.init.xavier_uniform_) for i in range(num_populations)]
 ga = DeterministicCrowdingGA(num_populations=num_populations,fitness_evaluator=evaluator, selection_pressure=0.1, 
               mutation_prob=1.0, mutation_power=0.001, crossover_prob=0.5)
-num_generations = 2000
+num_generations = 500
 solution, info = ga.run(populations=init_populations, num_generations=num_generations, num_workers=4, max_iterations=150, run_mode='multiprocess', visualize=False)
 
 # Learning curve
 plt.figure()
-plt.plot(range(num_generations), info.avg_fitnesses, label='AVG_FITNESS')
-plt.plot(range(num_generations), info.max_fitnesses, color='red', label='MAX_FITNESS')
+plt.plot(range(num_generations), np.clip(info.avg_fitnesses, a_min=-250, a_max=250), label='AVG')
+plt.plot(range(num_generations), np.clip(info.max_fitnesses, a_min=-250, a_max=250), color='red', label='MAX')
+plt.xlabel('Generation')
+plt.ylabel('Evaluation Reward')
 plt.legend()
-plt.savefig(f'{env_id.split(":")[1]}_crowding.png')
-positions_log = eval_logger.get_data()
-heatmap = np.zeros((64, 64))
+plt.savefig(f'./resources/{env_id.split(":")[1]}_crowding.png')
 
 # Heatmap
+bg_img = f'{env_id.split(":")[1].split("-")[1]}'
+bg = cv.resize(cv.imread(f'./resources/{bg_img}.png', 0), (64, 64))
+positions_log = eval_logger.get_data()
+heatmap = np.zeros((64, 64))
 for x,y in positions_log: heatmap[y][x] = min(heatmap[y][x] + 1, 500)
 plt.figure()
 plt.axis('off')
-plt.imshow(heatmap, cmap=plt.cm.Reds, interpolation='gaussian')
-plt.colorbar()
-plt.savefig(f'{env_id.split(":")[1]}_crowding_heatmap.png')
+plt.imshow(bg)
+plt.imshow(heatmap, cmap=plt.cm.Reds, interpolation='gaussian', alpha=0.3)
+plt.savefig(f'./resources/{env_id.split(":")[1]}_ga_crowding.png')
 
 ray.shutdown()
 
--- image_maze_ga.py
+++ image_maze_ga.py
@@ -3,6 +3,8 @@ import torch
 import ray
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import cv2 as cv
 
 from genetic_algorithm.genotype import TensorGenotype
 from genetic_algorithm.network_schema import *
@@ -10,6 +12,8 @@ from genetic_algorithm.ga import SimpleGA
 from genetic_algorithm.fitness_evaluator import GymFitnessEvaluator
 from environment.framestack_env_manager import FrameStackEnvManager
 from utilities.evaluation_logger import EvaluationLogger
+
+sns.set(style='darkgrid')
 
 def preprocess(screen):
     screen[screen < 255] = 0
@@ -41,28 +45,32 @@ network_schema = {
 ray.init()
 eval_logger = EvaluationLogger(get_log_data)
 evaluator = GymFitnessEvaluator(FrameStackEnvManager, eval_logger, env_name=env_id, preprocess=preprocess, frame_stack_size=frame_stack_size)
-init_populations = [TensorGenotype(network_schema, torch.nn.init.xavier_uniform_) for i in range(num_populations)]
-ga = SimpleGA(num_populations=num_populations,fitness_evaluator=evaluator, selection_pressure=0.2, 
-              mutation_prob=1.0, mutation_power=0.001, crossover_prob=0.5)
-num_generations = 2000
+init_populations = [TensorGenotype(network_schema, torch.nn.init.xavier_normal_) for i in range(num_populations)]
+ga = SimpleGA(num_populations=num_populations,fitness_evaluator=evaluator, selection_pressure=0.1, 
+              mutation_prob=0.1, mutation_power=0.002, crossover_prob=0.5)
+num_generations = 1000
 solution, info = ga.run(populations=init_populations, num_generations=num_generations, num_workers=4, max_iterations=150, run_mode='multiprocess', visualize=False)
 
 # Learning curve
 plt.figure()
-plt.plot(range(num_generations), info.avg_fitnesses, label='AVG_FITNESS')
-plt.plot(range(num_generations), info.max_fitnesses, color='red', label='MAX_FITNESS')
+plt.plot(range(num_generations), np.clip(info.avg_fitnesses, a_min=-250, a_max=250), label='AVG')
+plt.plot(range(num_generations), np.clip(info.max_fitnesses, a_min=-250, a_max=250), color='red', label='MAX')
+plt.xlabel('Generation')
+plt.ylabel('Evaluation Reward')
 plt.legend()
-plt.savefig(f'{env_id.split(":")[1]}_ga.png')
-positions_log = eval_logger.get_data()
-heatmap = np.zeros((64, 64))
+plt.savefig(f'./resources/{env_id.split(":")[1]}_ga.png')
 
 # Heatmap
+bg_img = f'{env_id.split(":")[1].split("-")[1]}'
+bg = cv.resize(cv.imread(f'./resources/{bg_img}.png', 0), (64, 64))
+positions_log = eval_logger.get_data()
+heatmap = np.zeros((64, 64))
 for x,y in positions_log: heatmap[y][x] = min(heatmap[y][x] + 1, 500)
 plt.figure()
 plt.axis('off')
-plt.imshow(heatmap, cmap=plt.cm.Reds, interpolation='gaussian')
-plt.colorbar()
-plt.savefig(f'{env_id.split(":")[1]}_ga_heatmap.png')
+plt.imshow(bg)
+plt.imshow(heatmap, cmap=plt.cm.Reds, interpolation='gaussian', alpha=0.3)
+plt.savefig(f'./resources/{env_id.split(":")[1]}_ga_heatmap.png')
 
 ray.shutdown()
 
