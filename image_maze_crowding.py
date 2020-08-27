68a308c9c48dec22039a6da7813253f3587bb8c6
thanakorn <thanakorn.panyapiang@outlook.com>
2020-08-27 15:10:13 +0100

    Fix plotting for crowding

--- image_maze_crowding.py
+++ image_maze_crowding.py
@@ -14,16 +14,16 @@ from utilities.image_maze_experiment import preprocess, get_log_data, num_action
 
 sns.set(style='darkgrid')
 
-env_id = 'gym_image_maze:ImageMaze-v0'
-num_populations = 60
-num_generations = 30
+env_id = 'gym_image_maze:ImageMaze-v2'
+num_populations = 100
+num_generations = 500
 
 ray.init()
 eval_logger = EvaluationLogger(get_log_data)
 evaluator = GymFitnessEvaluator(FrameStackEnvManager, eval_logger, env_name=env_id, preprocess=preprocess, frame_stack_size=frame_stack_size)
 init_populations = [TensorGenotype(network_schema, torch.nn.init.xavier_normal_) for i in range(num_populations)]
-ga = DeterministicCrowdingGA(num_populations=num_populations,fitness_evaluator=evaluator, selection_pressure=0.1, mutation_prob=0.2, mutation_power=0.002, crossover_prob=0.5)
-solution, info = ga.run(populations=init_populations, num_generations=num_generations, num_workers=4, max_iterations=150, run_mode='multiprocess', visualize=False)
+ga = DeterministicCrowdingGA(num_populations=num_populations,fitness_evaluator=evaluator, selection_pressure=0.1, mutation_prob=1.0, mutation_power=0.005, crossover_prob=0.5)
+solution, info = ga.run(populations=init_populations, num_generations=num_generations, num_workers=4, max_iterations=100, run_mode='multiprocess', visualize=False)
 
 evaluation_log = eval_logger.get_data()
 ray.shutdown()
@@ -32,8 +32,8 @@ positions_log = [pos for pos, _ in evaluation_log]
 distances_log = [-1 * dis for _, dis in evaluation_log]
 
 # Learning curve
-avg_dist = [np.mean(distances_log[i:(i + 1) * num_populations]) for i in range(num_generations)]
-max_dist = [np.max(distances_log[i:(i + 1) * num_populations]) for i in range(num_generations)]
+avg_dist = [np.mean(distances_log[i:(i + 1) * (num_populations * 2 )]) for i in range(num_generations)]
+max_dist = [np.max(distances_log[i:(i + 1) * (num_populations * 2)]) for i in range(num_generations)]
 plt.figure()
 plt.plot(range(num_generations), avg_dist, label='AVG')
 plt.plot(range(num_generations), max_dist, color='red', label='BEST')
@@ -48,7 +48,7 @@ plt.savefig(f'./resources/{env_id.split(":")[1]}_crowding.png')
 bg_img = f'{env_id.split(":")[1].split("-")[1]}'
 bg = cv.resize(cv.imread(f'./resources/{bg_img}.png', 0), (48, 48))
 heatmap = np.zeros((48, 48))
-for x,y in positions_log: heatmap[y][x] = min(heatmap[y][x] + 1, 350)
+for x,y in positions_log: heatmap[y][x] = min(heatmap[y][x] + 1, 50)
 plt.figure()
 plt.axis('off')
 plt.imshow(bg)
